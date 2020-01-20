d64a62e71c938312246dc8a4fed4852d7cc479e6
michaelrichey <michael@tofugu.com>
2020-01-20 10:20:30 -0500

    Added pizza maker site

---
+++ pizza-maker.js
@@ -0,0 +1,54 @@
+const ingredients = [
+    {
+      name: 'wine',
+      price: 'asdf'
+    },
+    {
+      name: 'brine',
+      price: 'asdf'
+    },
+    {
+      name: 'swine',
+      price: 'asdf'
+    },
+  ]
+  
+  
+  const message = function (x) {
+  
+    document.querySelector('#welcome').innerHTML = '<button id="submit-toppings">SUBMIT ORDER</button>'
+  
+    x.forEach(function (item) {
+      const generateText = document.createElement('p')
+      generateText.innerText = `You have added: ${item.name}`
+      document.querySelector("#welcome").appendChild(generateText)
+    })
+  }
+  
+  message(ingredients)
+  
+  document.querySelector("#form").addEventListener('submit', function (e) {
+    e.preventDefault()
+    ingredients.push(
+      {
+        name: e.target.elements.toppings.value,
+        price: 'asdf'
+      },
+    )
+    console.log(ingredients)
+    e.target.elements.toppings.value = ''
+    message(ingredients)
+  })
+  
+  document.querySelector("#submit-toppings").addEventListener("click", function (e) {
+    const finalOrder = document.createElement('p')
+    finalOrder.textContent = 'Your final pizza will have on top of it:'
+    document.querySelector('.box3').appendChild(finalOrder)
+  
+    ingredients.forEach(function (item) {
+      const toppingsList = document.createElement('p')
+      toppingsList.textContent = item.name
+      document.querySelector(".box3").appendChild(toppingsList)
+    })
+    console.log(ingredients)
+  })
