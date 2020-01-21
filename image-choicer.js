46c36595c5a8331bef0e45d95e035e98dc1c45de
michaelrichey <michael@tofugu.com>
2020-01-21 18:05:33 -0500

    add image select function

--- image-choicer.js
+++ image-choicer.js
@@ -1,3 +1,17 @@
 const test = document.createElement('p')
 test.textContent = 'test'
 document.querySelector("#picture-zone").appendChild(test)
+
+document.querySelector("#dropdown").addEventListener('change', function (e) {
+    console.log(e.target.value)
+    if (e.target.value === 'funky') {
+        console.log('its monkey')
+        document.querySelector("#picture-zone").innerHTML = '<img src="images/picture-choice-fk.png">'
+    } else if (e.target.value === 'tators') {
+        console.log('its gator')
+        document.querySelector("#picture-zone").innerHTML = '<img src="images/picture-choice-tators.jpg">'
+
+    } else {
+        console.log('something went wrong')
+    }
+})
