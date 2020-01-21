bcddae5f5155d99c7e2f6d32a369d7428f6a45eb
michaelrichey <michael@tofugu.com>
2020-01-21 18:15:54 -0500

    wrote notes about next steps

--- image-choicer.js
+++ image-choicer.js
@@ -15,3 +15,16 @@ document.querySelector("#dropdown").addEventListener('change', function (e) {
         console.log('something went wrong')
     }
 })
+
+// Can this be done with an array somehow? If we want to add 50 pictures, we need 50 if statements. Can we forEach over an array in some way to make this work? But then how can the event listener get the dropdown id? Add dropdown id to array. If e.target.value === object.id, then it adds innerHTML that grabs the img filename from the same spot in the array. Uuuh... how do I do that?
+
+/*
+if (e.target.value === object.id) {
+    document.querySelector("#picture-zone").innerHTML = `<img src="images/${object.img}">`
+} else {
+    console.log('something went wrong')
+}
+
+*/
+
+// But I still need some way for code to grab an image associated with a specific object in the array...
