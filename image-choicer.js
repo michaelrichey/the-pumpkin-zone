f196653e1bbc40fbfee8231e51772269d7ef4a15
michaelrichey <michael@tofugu.com>
2020-01-23 16:47:08 -0500

    Image choicer/add array (#5)
    * add array for images
    
    * add more images to dropdown
    
    * notes for new changes
    
    * add option generation js
    
    * remove redundant options
    
    * site formatting, add new placeholder
    
    * add images, bg animation
    
    * add links
    
    * fix blank dropdown option bug
    
    * added more images

--- image-choicer.js
+++ image-choicer.js
@@ -1,26 +1,101 @@
+const picList = [
+    {
+        filename: 'picture-choice-contra.png',
+        value: 'blank',
+        text: ''
+    },
+    {
+        filename: 'picture-choice-fk.png',
+        value: 'funky',
+        text: 'Funky Kong'
+    },
+    {
+        filename: 'picture-choice-tators.jpg',
+        value: 'tators',
+        text: 'Crunch Tators'
+    },
+    {
+        filename: 'picture-choice-contra.png',
+        value: 'contra',
+        text: 'Contra Hard Corps on Sega Genesis'
+    },
+    {
+        filename: 'picture-choice-ffix.png',
+        value: 'ffix',
+        text: 'Vivi fallen down in the street'
+    },
+    {
+        filename: 'picture-choice-nba-jam.jpg',
+        value: 'nba',
+        text: 'NBA Jam for Game Boy Box Back'
+    },
+    {
+        filename: 'picture-choice-pbcrisps.png',
+        value: 'pb',
+        text: 'PB Crisps by Planters'
+    },
+    {
+        filename: 'picture-choice-pepsi.jpg',
+        value: 'pepsi',
+        text: 'Pepsi Cool Cans 1990'
+    },
+    {
+        filename: 'picture-choice-train.jpg',
+        value: 'train',
+        text: 'An Exquisite Train'
+    },
+    {
+        filename: 'picture-choice-hikonyan.jpg',
+        value: 'hikonyan',
+        text: 'Hikonyan'
+    },
+    {
+        filename: 'picture-choice-taco-bell.jpeg',
+        value: 'tacobell',
+        text: 'Taco Bell Box Flying Through the Desert'
+    },
+    {
+        filename: 'picture-choice-shin-chan.png',
+        value: 'shinchanbento',
+        text: 'Crayon Shin-chan Bento'
+    },
+]
+
+picList.forEach(function (item) {
+
+    const generateOptions = document.createElement('option')
+    const setVal = document.createAttribute('value')
+    setVal.value = item.value
+    generateOptions.setAttributeNode(setVal)
+    generateOptions.innerHTML = item.text
+    document.querySelector("#dropdown").appendChild(generateOptions)
+
+})
+
 document.querySelector("#dropdown").addEventListener('change', function (e) {
     console.log(e.target.value)
-    if (e.target.value === 'funky') {
-        console.log('its monkey')
-        document.querySelector("#picture-zone").innerHTML = '<img src="images/picture-choice-fk.png">'
-    } else if (e.target.value === 'tators') {
-        console.log('its gator')
-        document.querySelector("#picture-zone").innerHTML = '<img src="images/picture-choice-tators.jpg">'
 
+    let getPic = picList.forEach(function (item) {
+        if (e.target.value === item.value) {
+            document.querySelector("#picture-zone").innerHTML = `<img src="images/${item.filename}">`
+        } else if (e.target.value === 'blank') {
+            document.querySelector("#picture-zone").innerHTML = `<img id="blank-image" src="images/${item.filename}">`
+        } else {
+            console.log('something went wrong')
+        }
+    })
+
+    getPic(picList)
+
+})
+
+let getPic = picList.forEach(function (item) {
+    if (item.value === 'blank') {
+        document.querySelector("#picture-zone").innerHTML = `<img id="blank-image" src="images/${item.filename}">`
     } else {
         console.log('something went wrong')
     }
 })
 
-// Can this be done with an array somehow? If we want to add 50 pictures, we need 50 if statements. Can we forEach over an array in some way to make this work? But then how can the event listener get the dropdown id? Add dropdown id to array. If e.target.value === object.id, then it adds innerHTML that grabs the img filename from the same spot in the array. Uuuh... how do I do that?
+getPic(picList)
 
-/*
-if (e.target.value === object.id) {
-    document.querySelector("#picture-zone").innerHTML = `<img src="images/${object.img}">`
-} else {
-    console.log('something went wrong')
-}
-
-*/
-
-// But I still need some way for code to grab an image associated with a specific object in the array...
