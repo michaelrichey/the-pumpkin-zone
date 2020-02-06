b29c9169db6b2b8d23f3dbb9658288712fe4a1b0
michaelrichey <michael@tofugu.com>
2020-02-06 14:58:37 -0500

    rearrange functions

--- bananas.js
+++ bananas.js
@@ -1,11 +1,31 @@
+window.addEventListener('load', function(){
+  console.log('5')
+  loader1();
+  document.getElementById('start').addEventListener('click', function() {
+    bananaMasher();
+  })
+});
 
+function loader1() {
+  console.log('4')
+  for (var i = 0; i < 5; i++) {
+    const p = document.createElement('p');
+    p.innerText = "Loaded " + i + " items...";
+    setTimeout(function() { document.body.appendChild(p) }, i * 1000);
+    if (i === 4) {
+      setTimeout(allDone, 5000);
+    }
+  }
+}
 
-function chomp() {
-  document.querySelectorAll('.banana')[0].remove();
-  alert('yum yum all done');
+function allDone() {
+  console.log('3')
+  alert('Done loading!');
+  document.getElementById('start').removeAttribute('disabled');
 }
 
 function bananaMasher() {
+  console.log('2')
   var max = maxBananas()
   for (var i = 1; i < max ; i++) {
     const p = document.createElement('p');
@@ -19,30 +39,14 @@ function bananaMasher() {
   }
 }
 
-function allDone() {
-  alert('Done loading!');
-  document.getElementById('start').removeAttribute('disabled');
-}
-
-function loader1() {
-  for (var i = 0; i < 5; i++) {
-    const p = document.createElement('p');
-    p.innerText = "Loaded " + i + " items...";
-    setTimeout(function() { document.body.appendChild(p) }, i * 1000);
-    if (i === 4) {
-      setTimeout(allDone, 5000);
-    }
-  }
-}
-
-window.addEventListener('load', function(){
-  loader1();
-  document.getElementById('start').addEventListener('click', function() {
-    bananaMasher();
-  })
-});
-
-
 function maxBananas() {
+  console.log('6')
   return Math.floor(Math.random() * 8) + 2;
+}
+
+
+function chomp() {
+  console.log('1')
+  document.querySelectorAll('.banana')[0].remove();
+  alert('yum yum all done');
 }
