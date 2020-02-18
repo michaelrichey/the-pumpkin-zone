be498243ef210dc7778eef9d0a8ef9155ebb5879
michaelrichey <michael@tofugu.com>
2020-02-18 16:57:29 -0500

    add html skeleton and js functions

--- character-select.js
+++ character-select.js
@@ -1,1 +1,9 @@
 console.log('hello there, butt')
+
+document.querySelector("#char1").addEventListener("click", function (e) {
+    console.log(e.target, 'it worked')
+    const makeImage = document.createElement("img")
+    makeImage.classList.add('top-img')
+    makeImage.src="images/picture-choice-nba-jam.jpg"
+    document.querySelector("#box1").appendChild(makeImage)
+})
