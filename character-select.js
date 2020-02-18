1051729c4c74883ef4cfe84b71982bf81d4b19b3
michaelrichey <michael@tofugu.com>
2020-02-18 17:03:39 -0500

    add picture switch functionality

--- character-select.js
+++ character-select.js
@@ -1,9 +1,21 @@
 console.log('hello there, butt')
 
 document.querySelector("#char1").addEventListener("click", function (e) {
+    document.querySelector("#box1").innerHTML = ''
+
     console.log(e.target, 'it worked')
     const makeImage = document.createElement("img")
     makeImage.classList.add('top-img')
     makeImage.src="images/picture-choice-nba-jam.jpg"
+    document.querySelector("#box1").appendChild(makeImage)
+})
+
+document.querySelector("#char2").addEventListener("click", function (e) {
+    document.querySelector("#box1").innerHTML = ''
+
+    console.log(e.target, 'it worked')
+    const makeImage = document.createElement("img")
+    makeImage.classList.add('top-img')
+    makeImage.src="images/picture-choice-shin-chan.png"
     document.querySelector("#box1").appendChild(makeImage)
 })
