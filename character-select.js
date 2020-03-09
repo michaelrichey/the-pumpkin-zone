fdc457d9c975f117d950ff121aa4be632dab9e8e
michaelrichey <michael@tofugu.com>
2020-03-09 11:41:07 -0400

    move image select functions to one function

--- character-select.js
+++ character-select.js
@@ -1,62 +1,37 @@
 const charList = [
-    {
-        name: "nba jam",
-        home: '"#char1"',
-        imgfile: '"images/picture-choice-nba-jam.jpg"'
-    },
-    {
-        name: "shin chan",
-        home: '"#char2"',
-        imgfile: '"images/picture-choice-shin-chan.png"'
-    },
-    {
-        name: "funky",
-        home: '"#char3"',
-        imgfile: '"images/picture-choice-fk.png"'
-    },
-        {
-        name: "pb crisps",
-        home: '"#char4"',
-        imgfile: '"images/picture-choice-pbcrisps.png"'
-    }
-]
+  {
+    name: "nba jam",
+    home: "char1",
+    imgfile: "images/picture-choice-nba-jam.jpg"
+  },
+  {
+    name: "shin chan",
+    home: "char2",
+    imgfile: "images/picture-choice-shin-chan.png"
+  },
+  {
+    name: "funky",
+    home: "char3",
+    imgfile: "images/picture-choice-fk.png"
+  },
+  {
+    name: "pb crisps",
+    home: "char4",
+    imgfile: "images/picture-choice-pbcrisps.png"
+  }
+];
 
-document.querySelector("#char1").addEventListener("click", function (e) {
-    document.querySelector("#box1").innerHTML = ''
+document.querySelectorAll(".selecto").forEach(function(item) {
+  item.addEventListener("click", function(e) {
+    charList.forEach(function(gizmo) {
+      if (gizmo.home === e.target.id) {
+        document.querySelector("#box1").innerHTML = "";
 
-    console.log(e.target, 'it worked')
-    const makeImage = document.createElement("img")
-    makeImage.classList.add('top-img')
-    makeImage.src="images/picture-choice-nba-jam.jpg"
-    document.querySelector("#box1").appendChild(makeImage)
-})
-
-document.querySelector("#char2").addEventListener("click", function (e) {
-    document.querySelector("#box1").innerHTML = ''
-
-    console.log(e.target, 'it worked')
-    const makeImage = document.createElement("img")
-    makeImage.classList.add('top-img')
-    makeImage.src="images/picture-choice-shin-chan.png"
-    document.querySelector("#box1").appendChild(makeImage)
-})
-
-document.querySelector("#char3").addEventListener("click", function (e) {
-    document.querySelector("#box1").innerHTML = ''
-
-    console.log(e.target, 'it worked')
-    const makeImage = document.createElement("img")
-    makeImage.classList.add('top-img')
-    makeImage.src="images/picture-choice-fk.png"
-    document.querySelector("#box1").appendChild(makeImage)
-})
-
-document.querySelector("#char4").addEventListener("click", function (e) {
-    document.querySelector("#box1").innerHTML = ''
-
-    console.log(e.target, 'it worked')
-    const makeImage = document.createElement("img")
-    makeImage.classList.add('top-img')
-    makeImage.src="images/picture-choice-pbcrisps.png"
-    document.querySelector("#box1").appendChild(makeImage)
-})
+        const makeImage = document.createElement("img");
+        makeImage.classList.add("top-img");
+        makeImage.src = gizmo.imgfile;
+        document.querySelector("#box1").appendChild(makeImage);
+      }
+    });
+  });
+});
