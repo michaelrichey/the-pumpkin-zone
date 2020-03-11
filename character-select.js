401dbe7400c297091dff59530dbc15e53ae5969d
michaelrichey <michael@tofugu.com>
2020-03-11 15:23:26 -0400

    add height and weight func

--- character-select.js
+++ character-select.js
@@ -15,19 +15,25 @@ const charList = [
     name: "shin chan",
     home: "char2",
     imgfile: "images/picture-choice-shin-chan.png",
-    headshot: "images/picture-choice-shin-chan.png"
+    headshot: "images/picture-choice-shin-chan.png",
+    height: "5' 2\"",
+    weight: "199 lbs"
   },
   {
     name: "funky",
     home: "char3",
     imgfile: "images/picture-choice-fk.png",
-    headshot: "images/picture-choice-fk.png"
+    headshot: "images/picture-choice-fk.png",
+    height: "4' 9\"",
+    weight: "167 lbs"
   },
   {
     name: "pb crisps",
     home: "char4",
     imgfile: "images/picture-choice-pbcrisps.png",
-    headshot: "images/picture-choice-pbcrisps.png"
+    headshot: "images/picture-choice-pbcrisps.png",
+    height: "5' 9\"",
+    weight: "187 lbs"
   }
 ];
 
@@ -36,10 +42,27 @@ document.querySelectorAll(".selecto").forEach(function (item) {
     makeFullBodyImgAppear(e)
     makeCharNameAppear(e)
     makeHeadshotImgAppear(e)
+    makeHeightWeight(e)
 
 
   });
 });
+
+
+const makeHeightWeight = function (event) {
+  charList.forEach(function (gizmo) {
+    if (gizmo.home === event.target.id) {
+      document.querySelector("ul").innerHTML = "";
+
+      const make = document.createElement('li')
+      make.textContent = `Height: ${gizmo.height}`
+      const make2 = document.createElement('li')
+      make2.textContent = `Weight: ${gizmo.weight}`
+      document.querySelector("ul").appendChild(make)
+      document.querySelector("ul").appendChild(make2)
+    }
+  })
+}
 
 const makeHeadshotImgAppear = function (event) {
   charList.forEach(function (gizmo) {
@@ -53,8 +76,6 @@ const makeHeadshotImgAppear = function (event) {
     }
   });
 }
-
-
 
 const makeCharNameAppear = function (event) {
   charList.forEach(function (gizmo) {
